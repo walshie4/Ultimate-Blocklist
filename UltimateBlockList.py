d7054e47dce135881c215cc092cd60bee7ab190f
Adam Walsh <adamwalsh14@gmail.com>
2014-07-06 03:47:47 -0400

    Now works, downloads all possible lists from iblocklist's site and combines them in one file

--- UltimateBlockList.py
+++ UltimateBlockList.py
@@ -6,6 +6,7 @@
 
 import requests
 from bs4 import BeautifulSoup as mksoup
+import gzip
 
 BASE = "https://www.iblocklist.com"
 
@@ -13,17 +14,26 @@ def get_value_from(url):
     soup = mksoup(requests.get(BASE + url).text)
     return str(soup.find_all("input")[-1]).split("\"")[-2]
 
-print("Getting list page")
-soup = mksoup(requests.get("https://www.iblocklist.com/lists.php").text)
-links = {}#dict of name of list -> its url
-for row in soup.find_all("tr")[1:]:#for each table row
-    section = str(list(row.children)[0])
-    pieces = section.split("\"")
-    links[pieces[2].split("<")[0][1:]] = pieces[1]
+def process(url):
+    contents = requests.get(url).text
+    f = open("blocklist.txt", "a+")
+    f.write(contents.encode("iso-8859-15", "replace"))
+    f.close()
 
-out = open("blocklist.txt", "w+")
-for link in links:#download and combine files
-    print "Downloading " + link + " blocklist."
-    value = get_value_from(links[link])
-    print value
+if __name__=="__main__":
+    print("Getting list page")
+    soup = mksoup(requests.get("https://www.iblocklist.com/lists.php").text)
+    links = {}#dict of name of list -> its url
+    for row in soup.find_all("tr")[1:]:#for each table row
+        section = str(list(row.children)[0])
+        pieces = section.split("\"")
+        links[pieces[2].split("<")[0][1:]] = pieces[1]
+
+    for link in links:#download and combine files
+        print "Downloading " + link + " blocklist."
+        value = get_value_from(links[link])
+        if value == "subscription":
+            print "Blocklist is not available for free download D:"
+        else:#download and add this sucker
+            process(value)
 
