10849047bd30a86b5f8b600f01fb96f11c4d52c8
Adam Walsh <adamwalsh14@gmail.com>
2014-07-06 02:48:24 -0400

    Added gathering of url to get list if available

--- UltimateBlockList.py
+++ UltimateBlockList.py
@@ -7,6 +7,12 @@
 import requests
 from bs4 import BeautifulSoup as mksoup
 
+BASE = "https://www.iblocklist.com"
+
+def get_value_from(url):
+    soup = mksoup(requests.get(BASE + url).text)
+    return str(soup.find_all("input")[-1]).split("\"")[-2]
+
 print("Getting list page")
 soup = mksoup(requests.get("https://www.iblocklist.com/lists.php").text)
 links = {}#dict of name of list -> its url
@@ -15,5 +21,9 @@ for row in soup.find_all("tr")[1:]:#for each table row
     pieces = section.split("\"")
     links[pieces[2].split("<")[0][1:]] = pieces[1]
 
-print links
+out = open("blocklist.txt", "w+")
+for link in links:#download and combine files
+    print "Downloading " + link + " blocklist."
+    value = get_value_from(links[link])
+    print value
 
